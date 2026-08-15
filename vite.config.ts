32d21bb3bbe6a4ba283b375fbbe9a0bbb3dcd1f4
reamkf <68076678+reamkf@users.noreply.github.com>
2026-08-15 13:08:39 +0900

    fix: enable HMR for SSR updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import honox from "honox/vite";
+import { defaultOptions as devServerDefaultOptions } from "@hono/vite-dev-server";
 import { defineConfig } from "vite";
 import adapter from "@hono/vite-dev-server/cloudflare";
 import build from "@hono/vite-build/cloudflare-workers";
@@ -13,7 +14,10 @@ export default defineConfig({
       client: {
         input: ["/app/client.ts", "/app/style.css"],
       },
-      devServer: { adapter },
+      devServer: {
+        adapter,
+        handleHotUpdate: devServerDefaultOptions.handleHotUpdate,
+      },
     }),
     build(),
   ],
